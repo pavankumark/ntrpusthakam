e726c80a2e2b45754ccc84d54708554ee356f2b7
Pavan Kumar <pavan@reachall.in>
2014-07-17 22:37:30 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ntrpusthakam::Application.config.secret_token = '7d2c8f00d065266c01bd2dc4377accdc19885d032c21fd93790049746415b0881e9fc9f684cc9723dc20342e7e4841879b943dd91effd2fb5b6c2a0aa1300b59'
